58f6473c7a7bf81586c26dc5dd0179b7f0f9abe2
jz078 <110040908+jz078@users.noreply.github.com>
2023-12-28 23:26:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert("hello world");
+
+console.log("the code is looking like a wow...");
+// let a = prompt("Enter your number");
+// console.log("your number is ",a);
+let istrue = confirm("do you want to leave this page ?");
+if(istrue){
+    alert("your phone will blast if you leave this page ");
+    let istrue1 = confirm("confirm to leave ? ");
+    if(istrue1) alert("phone is blasting...");
+    else alert("you saved your phone");
+}
